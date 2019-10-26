56342e0bf24dafdbb876131a455151fb89f9ddd7
xuexin <tomyail1989@gmail.com>
2019-10-26 23:09:19 +0800

    ecto changeset

--- apps/auction/lib/auction/item.ex
+++ apps/auction/lib/auction/item.ex
@@ -1,10 +1,17 @@
 defmodule Auction.Item do
- use Ecto.Schema
+  use Ecto.Schema
 
- schema "items" do
-  field :title, :string
-  field :description, :string
-  field :ends_at, :utc_datetime
-  timestamps()
- end
+  schema "items" do
+    field(:title, :string)
+    field(:description, :string)
+    field(:ends_at, :utc_datetime)
+    timestamps()
+  end
+
+  def changeset(item, params \\ %{}) do
+    item
+    |> Ecto.Changeset.cast(params, [:title, :description, :ends_at])
+    |> Ecto.Changeset.validate_required(:title)
+    |> Ecto.Changeset.validate_length(:title, min: 3)
+  end
 end
