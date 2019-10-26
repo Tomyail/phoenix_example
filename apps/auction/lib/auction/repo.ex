f23851b12bf6838de9f4b77eedb932a856e9c7a5
xuexin <tomyail1989@gmail.com>
2019-10-26 22:37:32 +0800

    using ecto to connect db

--- apps/auction/lib/auction/repo.ex
+++ apps/auction/lib/auction/repo.ex
@@ -1,38 +1,42 @@
 defmodule Auction.Repo do
-  alias Auction.Item
+  # alias Auction.Item
 
-  @item [
-    %Item{
-      id: 1,
-      title: "My First item",
-      description: "A taske item",
-      ends_at: ~N[2020-02-02 01:00:00]
-    },
-    %Item{
-      id: 2,
-      title: "My First 2",
-      description: "A taske item2",
-      ends_at: ~N[2020-02-02 01:00:00]
-    },
-    %Item{
-      id: 3,
-      title: "My First item3",
-      description: "A taske item3",
-      ends_at: ~N[2020-02-02 01:00:00]
-    }
-  ]
+  # @item [
+  #   %Item{
+  #     id: 1,
+  #     title: "My First item",
+  #     description: "A taske item",
+  #     ends_at: ~N[2020-02-02 01:00:00]
+  #   },
+  #   %Item{
+  #     id: 2,
+  #     title: "My First 2",
+  #     description: "A taske item2",
+  #     ends_at: ~N[2020-02-02 01:00:00]
+  #   },
+  #   %Item{
+  #     id: 3,
+  #     title: "My First item3",
+  #     description: "A taske item3",
+  #     ends_at: ~N[2020-02-02 01:00:00]
+  #   }
+  # ]
 
-  def all(Item), do: @item
+  # def all(Item), do: @item
 
-  def get!(Item, id) do
-    Enum.find(@item, fn item -> item.id === id end)
-  end
+  # def get!(Item, id) do
+  #   Enum.find(@item, fn item -> item.id === id end)
+  # end
 
-  def get_by(Item, attrs) do
-    Enum.find(@item, fn item ->
-      Enum.all?(Map.keys(attrs), fn key ->
-        Map.get(item, key) === attrs[key]
-      end)
-    end)
-  end
+  # def get_by(Item, attrs) do
+  #   Enum.find(@item, fn item ->
+  #     Enum.all?(Map.keys(attrs), fn key ->
+  #       Map.get(item, key) === attrs[key]
+  #     end)
+  #   end)
+  # end
+
+  use Ecto.Repo,
+    otp_app: :auction,
+    adapter: Ecto.Adapters.Postgres
 end
